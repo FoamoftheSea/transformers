Fixed cardinality loss calculation to use score threshold. Fixed post-processing in image_processor.

src/transformers/models/multiformer/image_processing_multiformer.py
@@ -21,6 +21,7 @@ from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Tuple, Un
 
 import numpy as np
 
+from ..detr.modeling_detr import DetrObjectDetectionOutput
 from ...feature_extraction_utils import BatchFeature
 from ...image_processing_utils import BaseImageProcessor, get_size_dict
 from ...image_transforms import (
@@ -469,6 +470,91 @@ def score_labels_from_class_probabilities(logits: np.ndarray) -> Tuple[np.ndarra
     scores = np.take_along_axis(probs, labels, axis=-1)
     scores, labels = scores.squeeze(-1), labels.squeeze(-1)
     return scores, labels
+
+
+# Copied from transformers.models.deformable_detr.image_processing_deformable_detr
+def post_process_object_detection(
+    outputs,
+    threshold: float = 0.5,
+    target_sizes: Union[TensorType, List[Tuple]] = None,
+    top_k: int = 100,
+    boxes_only: bool = False,
+):
+    """
+    Converts the raw output of [`DeformableDetrForObjectDetection`] into final bounding boxes in (top_left_x,
+    top_left_y, bottom_right_x, bottom_right_y) format. Only supports PyTorch.
+
+    Args:
+        outputs ([`DetrObjectDetectionOutput`, `dict`]):
+            Raw outputs of the model.
+        threshold (`float`, *optional*):
+            Score threshold to keep object detection predictions.
+        target_sizes (`torch.Tensor` or `List[Tuple[int, int]]`, *optional*):
+            Tensor of shape `(batch_size, 2)` or list of tuples (`Tuple[int, int]`) containing the target size
+            (height, width) of each image in the batch. If left to None, predictions will not be resized.
+        top_k (`int`, *optional*, defaults to 100):
+            Keep only top k bounding boxes before filtering by thresholding.
+        boxes_only (`bool`, *optional*, defaults to False):
+            When True, just pass a list or tensor of boxes to be coverted and resized.
+
+    Returns:
+        `List[Dict]`: A list of dictionaries, each dictionary containing the scores, labels and boxes for an image
+        in the batch as predicted by the model.
+    """
+    if not boxes_only:
+        if isinstance(outputs, DetrObjectDetectionOutput):
+            out_logits, out_bbox = outputs.logits, outputs.pred_boxes
+        elif isinstance(outputs, dict):
+            out_logits, out_bbox = outputs["logits"], outputs["pred_boxes"]
+        else:
+            raise ValueError("'outputs' must be dict or DetrObjectDetectionOutput type.")
+
+        out_logits = torch.FloatTensor(out_logits)
+
+        if target_sizes is not None:
+            if len(out_logits) != len(target_sizes):
+                raise ValueError(
+                    "Make sure that you pass in as many target sizes as the batch dimension of the logits"
+                )
+
+        prob = out_logits.sigmoid()
+        prob = prob.view(out_logits.shape[0], -1)
+        k_value = min(top_k, prob.size(1))
+        topk_values, topk_indexes = torch.topk(prob, k_value, dim=1)
+        scores = topk_values
+        topk_boxes = torch.div(topk_indexes, out_logits.shape[2], rounding_mode="floor")
+        labels = topk_indexes % out_logits.shape[2]
+
+    else:
+        out_bbox = outputs
+
+    out_bbox = torch.Tensor(out_bbox)
+    boxes = center_to_corners_format(out_bbox)
+    if not boxes_only:
+        boxes = torch.gather(boxes, 1, topk_boxes.unsqueeze(-1).repeat(1, 1, 4))
+
+    # and from relative [0, 1] to absolute [0, height] coordinates
+    if target_sizes is not None:
+        if isinstance(target_sizes, List):
+            img_h = torch.Tensor([i[0] for i in target_sizes])
+            img_w = torch.Tensor([i[1] for i in target_sizes])
+        else:
+            img_h, img_w = target_sizes.unbind(1)
+        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1).to(boxes.device)
+        boxes = boxes * scale_fct[:, None, :]
+
+    results = []
+
+    if not boxes_only:
+        for s, l, b in zip(scores, labels, boxes):
+            score = s[s > threshold]
+            label = l[s > threshold]
+            box = b[s > threshold]
+            results.append({"scores": score, "labels": label, "boxes": box})
+    else:
+        results = [{"boxes": box} for box in boxes]
+
+    return results
 
 
 # Copied from transformers.models.detr.image_processing_detr.post_process_panoptic_sample
@@ -1392,63 +1478,5 @@ class MultiformerImageProcessor(BaseImageProcessor):
         boxes = boxes * scale_fct[:, None, :]
 
         results = [{"scores": s, "labels": l, "boxes": b} for s, l, b in zip(scores, labels, boxes)]
-
-        return results
-
-    def post_process_object_detection(
-        self, outputs, threshold: float = 0.5, target_sizes: Union[TensorType, List[Tuple]] = None, top_k: int = 100
-    ):
-        """
-        Converts the raw output of [`DeformableDetrForObjectDetection`] into final bounding boxes in (top_left_x,
-        top_left_y, bottom_right_x, bottom_right_y) format. Only supports PyTorch.
-
-        Args:
-            outputs ([`DetrObjectDetectionOutput`]):
-                Raw outputs of the model.
-            threshold (`float`, *optional*):
-                Score threshold to keep object detection predictions.
-            target_sizes (`torch.Tensor` or `List[Tuple[int, int]]`, *optional*):
-                Tensor of shape `(batch_size, 2)` or list of tuples (`Tuple[int, int]`) containing the target size
-                (height, width) of each image in the batch. If left to None, predictions will not be resized.
-            top_k (`int`, *optional*, defaults to 100):
-                Keep only top k bounding boxes before filtering by thresholding.
-
-        Returns:
-            `List[Dict]`: A list of dictionaries, each dictionary containing the scores, labels and boxes for an image
-            in the batch as predicted by the model.
-        """
-        out_logits, out_bbox = outputs.logits, outputs.pred_boxes
-
-        if target_sizes is not None:
-            if len(out_logits) != len(target_sizes):
-                raise ValueError(
-                    "Make sure that you pass in as many target sizes as the batch dimension of the logits"
-                )
-
-        prob = out_logits.sigmoid()
-        prob = prob.view(out_logits.shape[0], -1)
-        k_value = min(top_k, prob.size(1))
-        topk_values, topk_indexes = torch.topk(prob, k_value, dim=1)
-        scores = topk_values
-        topk_boxes = torch.div(topk_indexes, out_logits.shape[2], rounding_mode="floor")
-        labels = topk_indexes % out_logits.shape[2]
-        boxes = center_to_corners_format(out_bbox)
-        boxes = torch.gather(boxes, 1, topk_boxes.unsqueeze(-1).repeat(1, 1, 4))
-
-        # and from relative [0, 1] to absolute [0, height] coordinates
-        if isinstance(target_sizes, List):
-            img_h = torch.Tensor([i[0] for i in target_sizes])
-            img_w = torch.Tensor([i[1] for i in target_sizes])
-        else:
-            img_h, img_w = target_sizes.unbind(1)
-        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1).to(boxes.device)
-        boxes = boxes * scale_fct[:, None, :]
-
-        results = []
-        for s, l, b in zip(scores, labels, boxes):
-            score = s[s > threshold]
-            label = l[s > threshold]
-            box = b[s > threshold]
-            results.append({"scores": score, "labels": label, "boxes": box})
 
         return results

src/transformers/models/multiformer/metrics_multiformer.py
@@ -6,6 +6,7 @@ import torch
 from torchmetrics.detection import MeanAveragePrecision
 
 from transformers import EvalPrediction
+from transformers.models.multiformer.image_processing_multiformer import post_process_object_detection
 from transformers.models.multiformer.modeling_multiformer import MultiformerTask, IRMSELoss, SiLogLoss
 
 
@@ -107,16 +108,18 @@ class MultiformerMetric:
         id2label: Optional[Dict[int, str]] = None,
         ignore_class_ids: Optional[Set[int]] = None,
         reduced_labels: bool = False,
+        box_score_threshold: float = 0.35,
     ):
         self.id2label = id2label
         self.ignore_class_ids = ignore_class_ids
         self.reduced_labels = reduced_labels
+        self.box_score_threshold = box_score_threshold
         self.metrics = {}
         self.reset_metrics()
 
     def reset_metrics(self):
         self.metrics = {
-            "det_2d": MeanAveragePrecision(max_detection_thresholds=[3, 30, 300]),
+            "det_2d": MeanAveragePrecision(),
             "semseg": MultiformerSemanticEvalMetric(
                 id2label=self.id2label,
                 ignore_class_ids=self.ignore_class_ids,
@@ -127,21 +130,21 @@ class MultiformerMetric:
 
     def convert_eval_pred(self, eval_pred, task):
         if task == MultiformerTask.DET_2D:
-            preds = []
-            for i in range(eval_pred.predictions["logits"].shape[0]):
-                prediction_scores = torch.softmax(torch.FloatTensor(eval_pred.predictions["logits"][i]), dim=-1).max(1)
-                preds.append(
-                    {
-                        "boxes": torch.FloatTensor(eval_pred.predictions["pred_boxes"][i]),
-                        "scores": prediction_scores[0],
-                        "labels": torch.LongTensor(prediction_scores[1]),
-                        # "masks": None,
-                    }
-                )
+            target_sizes = [eval_pred.inputs[i, 0].shape for i in range(eval_pred.inputs.shape[0])]
+            preds = post_process_object_detection(
+                eval_pred.predictions,
+                threshold=self.box_score_threshold,
+                target_sizes=target_sizes
+            )
 
             target = [
                 {
-                    "boxes": torch.FloatTensor(eval_pred.label_ids["labels"][i]["boxes"]),
+                    "boxes": post_process_object_detection(
+                        eval_pred.label_ids["labels"][i]["boxes"][None, ...],
+                        threshold=self.box_score_threshold,
+                        target_sizes=[target_sizes[i]],
+                        boxes_only=True,
+                    )[0]["boxes"],
                     "labels": torch.LongTensor(eval_pred.label_ids["labels"][i]["class_labels"]),
                     # "masks": None,
                     # "iscrowd": None,